6d494fd7b4b3a8a1169e650a44336f7565130dc9
Kefli Yueksel <kefliyuksel@gmail.com>
2022-06-05 09:46:19 +0200

    Initial commit

---
+++ src/main/java/com/countries/server/WsCountriesServerApplication.java
@@ -0,0 +1,13 @@
+package com.countries.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsCountriesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsCountriesServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/countries/server/WsCountriesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.countries.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsCountriesServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
